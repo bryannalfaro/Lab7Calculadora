125c546d887a177184d5a6ae5f50f6664a00ad45
Bryann Alfaro <alf19372@uvg.edu.gt>
2021-04-19 11:24:13 -0600

    front calculator

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,19 +5,29 @@ import '../../styles/app.css'
 
 const App = () => (
   <div className="super">
-    <h5>WEB CALCULATOR</h5>
+    <h5 className="titleCalculator">WEB CALCULATOR</h5>
     <div className="padre">
       <Pantalla />
       <div className="buttons">
-        <Button texto="1" />
-        <Button texto="2" />
-        <Button texto="3" />
-        <Button texto="4" />
-        <Button texto="5" />
-        <Button texto="6" />
+        <Button texto="C" />
+        <Button texto="+/-" />
+        <Button texto="%" />
+        <Button texto="/" />
         <Button texto="7" />
         <Button texto="8" />
         <Button texto="9" />
+        <Button texto="x" />
+        <Button texto="4" />
+        <Button texto="5" />
+        <Button texto="6" />
+        <Button texto="-" />
+        <Button texto="1" />
+        <Button texto="2" />
+        <Button texto="3" />
+        <Button texto="+" />
+        <Button texto="0" />
+        <Button texto="." />
+        <Button classe="equal" texto="=" />
       </div>
     </div>
   </div>
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import '../../styles/Button.css'
 import PropTypes from 'prop-types'
 
-const Button = ({ texto }) => (
-  <button type="button">{texto}</button>
+const Button = ({ classe, texto }) => (
+  <button className={classe} type="button">{texto}</button>
 )
 
 Button.propTypes = {
   texto: PropTypes.string.isRequired,
+  classe: PropTypes.string.isRequired,
 }
 
 export default Button
