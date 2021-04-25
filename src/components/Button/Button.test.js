221c5be7041411b1a03380aadfa40082db48c431
Bryann Alfaro <alf19372@uvg.edu.gt>
2021-04-25 11:29:06 -0600

    test fixed

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { screen, render } from '@testing-library/react/'
+import userEvent from '@testing-library/user-event'
 import Button from './Button'
 
 describe('Tests', () => {
@@ -11,8 +12,11 @@ describe('Tests', () => {
     expect(screen.getByText('+')).toBeInTheDocument()
   })
 
-  test('Button can be clicked', () => {
-    render(<Button clicke={jest.fn()} classe="Dummy" texto="+" />)
-    expect(screen.getByText('+')).toBeInTheDocument()
+  test('Button can be clicked calling an spy function', () => {
+    const mock = jest.fn()
+    const { container } = render(<Button clicke={mock} classe="Dummy" texto="+" />)
+    userEvent.click(container.firstChild)
+    userEvent.click(container.firstChild)
+    expect(mock).toHaveBeenCalledTimes(2)
   })
 })
